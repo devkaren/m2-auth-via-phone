6d0bcce840eddc8848bcee7ae87a01bf086ed1fd
developer_karen <developerkarensimonyan@gmail.com>
2021-01-06 03:11:28 +0400

    Initial commit

---
+++ Block/Form/Login.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Customer\Module\Block;
+
+class Login extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Construct
+     *
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template $context,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Get form action URL for POST booking request
+     *
+     * @return string
+     */
+    public function getFormAction()
+    {
+        // companymodule is given in routes.xml
+        // controller_name is folder name inside controller folder
+        // action is php file name inside above controller_name folder
+
+        return '/companymodule/controller_name/action';
+        // here controller_name is index, action is booking
+    }
+}
