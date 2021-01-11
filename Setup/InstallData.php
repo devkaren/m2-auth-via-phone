812457ebdf3c0af455c358caa61e496d796d2e06
developer_karen <developerkarensimonyan@gmail.com>
2021-01-11 12:11:43 +0400

    Update "lastname" and "firstname" field as no "required"

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -2,12 +2,12 @@
 
 namespace Customer\PhoneAuthentication\Setup;
 
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Config;
 use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Eav\Model\Config;
-use Magento\Customer\Model\Customer;
 
 class InstallData implements InstallDataInterface
 {
@@ -23,7 +23,7 @@ class InstallData implements InstallDataInterface
     {
         $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
         $eavSetup->addAttribute(
-            \Magento\Customer\Model\Customer::ENTITY,
+            Customer::ENTITY,
             'phone_number',
             [
                 'type'         => 'varchar',
@@ -36,6 +36,10 @@ class InstallData implements InstallDataInterface
                 'system'       => 0,
             ]
         );
+        // Update firstname and lastname field as "no required"
+        $eavSetup->updateAttribute(Customer::ENTITY, 'firstname', 'is_required', 'false');
+        $eavSetup->updateAttribute(Customer::ENTITY, 'lastname', 'is_required', 'false');
+
         $attribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'phone_number');
 
         $attribute->setData('used_in_forms', ['adminhtml_customer', 'customer_account_create', 'customer_account_edit']);
