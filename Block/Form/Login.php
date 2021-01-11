c688ff8a86813814b995338288c802f79c9898a9
developer_karen <developerkarensimonyan@gmail.com>
2021-01-11 07:27:32 +0400

    Adding "Login" Block

--- Block/Form/Login.php
+++ Block/Form/Login.php
@@ -2,21 +2,8 @@
 
 namespace Customer\PhoneAuthentication\Block\Form;
 
-class Login extends \Magento\Framework\View\Element\Template
+class Login extends \Magento\Customer\Block\Form\Login
 {
-    /**
-     * Construct
-     *
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Framework\View\Element\Template $context,
-        array $data = []
-    )
-    {
-        parent::__construct($context, $data);
-    }
 
     /**
      * Get form action URL for POST booking request
@@ -25,12 +12,6 @@ class Login extends \Magento\Framework\View\Element\Template
      */
     public function getFormActionUrl()
     {
-        // companymodule is given in routes.xml
-        // controller_name is folder name inside controller folder
-        // action is php file name inside above controller_name folder
-
-//        return '/companymodule/controller_name/action';
-        return $this->urlBuilder->getUrl('customer/account/loginPost');
-        // here controller_name is index, action is booking
+        return $this->_customerUrl->getLoginPostUrl();
     }
 }
